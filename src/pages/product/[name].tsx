aeecff96510a3743febd34aee5aa14d50a66230f
Louis-Henri Mc Allister <louis-henri.mc-allister@epitech.eu>
2024-11-05 23:39:50 +0100

    fix: loader page

--- src/pages/product/[name].tsx
+++ src/pages/product/[name].tsx
@@ -1,19 +1,12 @@
-import { useRouter } from "next/router";
 import { Fragment } from "react";
 import Head from "next/head";
 import Products from "src/data/Products";
 import ProductDetail from "src/views/products/details/ProductDetail";
+import ProductTypes from "src/types/ProductTypes";
 
+const Product = ({ product }: { product: ProductTypes }) => {
+  if (!product) return <h1>Article not found</h1>;
 
-const Product = () => {
-  const router = useRouter();
-  const { name } = router.query;
-
-  const product = Products.find((product) => product.link === (name + '/'));
-
-  if (!product) return (
-    <h1>Article not found</h1>
-  );
   return (
     <Fragment>
       <Head>
@@ -23,6 +16,17 @@ const Product = () => {
       <ProductDetail product={product} />
     </Fragment>
   );
+};
+
+export async function getServerSideProps(context: { query: { name: any; }; }) {
+  const { name } = context.query;
+  const product = Products.find((product) => product.link === (name + '/'));
+
+  return {
+    props: {
+      product: product || null,
+    },
+  };
 }
 
 export default Product;
